1d754a30707e90fd8dacece85a4c6a5c537c7c0c
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-03-18 15:05:12 +0100

    added hbs view engine and vars in .hbs file

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 port = 3000;
 
 const app = express();
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {  // homepage, could also be subpage, like /o-nas
-    res.send('Hello node, again!');
+    res.render('index', {
+        pageTitle: 'Start',
+        pageBody: 'heloł'
+    });
 })
 
 app.get('/kontakt', (req, res) => {
