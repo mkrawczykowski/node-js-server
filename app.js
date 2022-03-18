0e1cd8bf6b24be2d2a56878b056aa76d8e32bc7f
Michal Krawczykowski <mkrawczykowski@gmail.com>
2022-03-18 12:52:29 +0100

    working server from scratch

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const utils = require('./utils');
+const http = require('http');
+const port = 3000;
+const handler = (request, response) => {
+    console.log('new user!');
+    response.end('Hello node!');
+}
 
-utils.greeting();
-utils.add(3,4);
+const server = http.createServer(handler);
+server.listen(port, (err) => {
+    if (err){
+        console.log('Damn! Something went wrong :(');
+    }
+
+    console.log('Server is running...');
+});
